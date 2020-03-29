e0c71ad13850851dcbe60c153168fe64aaad2460
soon <42361070+soonlabo@users.noreply.github.com>
2020-03-29 12:29:37 +0900

    ソースをクリーン

--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Linq;
-
+ 
 namespace FizzBuzz
 {
     class Program
     {
-        /// <summary>
-        /// FizzBuzz
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
             foreach (int currentNum in Enumerable.Range(1,100))
@@ -31,53 +27,26 @@ namespace FizzBuzz
                 }
             }
         }
-
-        /// <summary>
-        /// 3で割り切れるか判定
-        /// </summary>
-        /// <param name="currentNum">現在値</param>
-        /// <returns></returns>
+ 
+        // 3で割り切れるか判定
         private static bool Fizz(int currentNum)
         {
-            if (currentNum % 3 == 0)
-            {
-                return true;
-            }
-            return false;
+            return currentNum % 3 == 0;
         }
-
-        /// <summary>
-        /// 5で割り切れるか判定
-        /// </summary>
-        /// <param name="currentNum">現在値</param>
-        /// <returns></returns>
+ 
+        // 5で割り切れるか判定
         private static bool Buzz(int currentNum)
         {
-            if (currentNum % 5 == 0)
-            {
-                return true;
-            }
-            return false;
+            return currentNum % 5 == 0;
         }
-
-        /// <summary>
-        /// 15で割り切れるか判定
-        /// </summary>
-        /// <param name="currentNum">現在値</param>
-        /// <returns></returns>
+ 
+        // 15で割り切れるか判定
         private static bool FizzBuzz(int currentNum)
         {
-            if (currentNum % 15 == 0)
-            {
-                return true;
-            }
-            return false;
+            return currentNum % 15 == 0;
         }
-
-        /// <summary>
-        /// コンソールに表示
-        /// </summary>
-        /// <param name="value">表示する値</param>
+ 
+        // コンソールに表示
         private static void ConsoleDisplay(string value)
         {
             Console.WriteLine(value);
